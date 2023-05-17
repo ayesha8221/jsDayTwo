6aba2eccb20f461ce431395e2cb1f0a0de25b34f
Ayesha <ayesha.glnt012@gmail.com>
2023-05-17 21:18:58 +0200

    adding files

---
+++ script/code.js
@@ -0,0 +1,20 @@
+const subjects= ["Python", "C++","VB.Net", "Javascript", "Assembly"];
+
+// at() method takes an integer value
+// Negative numbers reverse the count in the array
+console.log(subjects.at(-1));
+// output= Assembley
+
+// indexes in an array start from 0
+subjects[0]= "Python";
+subjects[1]= "C++";
+subjects[2]= "VB.Net";
+subjects[3]='Javascript';
+subjects[4]='Assembley';
+console.log(subjects[2]);
+
+// output= VB.Net
+
+
+// now the real work starts because I have to push and merge
+// Thank u Oslin
