47d0c6c4b6c978478fd638dcfe3475c345d80c8b
ayesha8221 <ayesha.glnt012@gmail.com>
2023-05-18 16:33:36 +0200

    loops task

---
+++ script/code.js
@@ -0,0 +1,55 @@
+// document.write('<h2> 1 - 10</h2>')
+// for (let i=0; i<=10; i++)
+// {
+//     document.write(i+"<br/>");
+// }
+
+// document.write("<h2> 2's </h2>")
+// for (let i=0; i<=10; i+=2)
+// {
+//     document.write(i+"<br/>");
+// }
+
+// document.write('<h2> Reverse twos </h2>')
+// for (let i=10; i>0; i-=2)
+// {
+//     document.write(i+"<br/>");
+// }
+
+// document.write("<h2> 7's Multiplication Table</h2>")
+// for (let i=1; i<=10; i++)
+// {
+//     let sum= i*7
+//     document.write(i+"x"+'7'+sum+"<br/>");
+//     if(sum==49)
+//     break;
+// }
+
+// document.write("<h2> 7's Multiplication Table</h2>")
+// for (let i=1; i<=10; i++)
+// {
+//     let sum= i*7
+//     document.write(i+"x"+'7'+sum+"<br/>");
+//     if(sum==49)
+//     continue;
+// }
+
+document.write("<h2> Task 1 <h2/>");
+
+function myFunction(){
+    let 
+}
+for (let i=1; i<=20; i+=2)
+{
+    document.write(i+" is odd " + "<br/>")
+}
+
+for (let i=2; i<=20; i+=2)
+{
+    document.write(i+" is even " + "<br/>")
+}
+
+
+
+
+
